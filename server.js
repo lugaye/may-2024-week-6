1d2b1a377bfebc4df0b51954f9991a3b0383e9cf
lugaye <160602222+lugaye@users.noreply.github.com>
2024-07-23 20:14:35 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,146 +1,164 @@
-// server.js
 const express = require('express');
+const mysql = require('mysql2');
 const session = require('express-session');
 const bcrypt = require('bcryptjs');
 const bodyParser = require('body-parser');
-const mysql = require('mysql2');
+const path = require('path');
 const { check, validationResult } = require('express-validator');
+
+
+//initialize
 const app = express();
 
-// Configure session middleware
-app.use(session({
-    secret: 'secret-key',
-    resave: false,
-    saveUninitialized: true
-}));
-
-// Create MySQL connection
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'learning_management'
-});
-
-// Connect to MySQL
-connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to MySQL: ' + err.stack);
-        return;
-    }
-    console.log('Connected to MySQL as id ' + connection.threadId);
-});
-
-// Set up middleware to parse incoming data
+//configure - middleware
+app.use(express.static(__dirname));
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Define routes
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+// Configure session middleware
+app.use(session({
+    secret: 'uwebuiwebciuwebcwecubweubweofbweofbowebfouwbfuowerb',
+    resave: false,
+    saveUninitialized: false,
+}));
+
+//create connection
+const connection  = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'plp_expense'
 });
 
+connection.connect((err) => {
+    if(err){
+        console.error('Error occured while connecting to the db server: ' + err.stack);
+        return;
+    }
+    console.log('DB Server connected successfully.');
+});
 
-  
-// Define a User representation for clarity
+//define route to registration form
+app.get('/register', (request, response) => {
+    response.sendFile(path.join(__dirname, 'register.html'));
+});
+
+//display login page
+app.get('/login', (request, response) => {
+    response.sendFile(path.join(__dirname, "login.html"));
+});
+
+//define a User object - registration
 const User = {
-    tableName: 'users', 
-    createUser: function(newUser, callback) {
+    tableName: 'users',
+    createUser: function(newUser, callback){
         connection.query('INSERT INTO ' + this.tableName + ' SET ?', newUser, callback);
-    },  
-    getUserByEmail: function(email, callback) {
+    },
+    getUserByEmail: function(email, callback){
         connection.query('SELECT * FROM ' + this.tableName + ' WHERE email = ?', email, callback);
     },
-    getUserByUsername: function(username, callback) {
+    getUserByUsername: function(username, callback){
         connection.query('SELECT * FROM ' + this.tableName + ' WHERE username = ?', username, callback);
-    }
-};
+    },
+}
 
-// Registration route
-app.post('/register', [
-    // Validate email and username fields
-    check('email').isEmail(),
-    check('username').isAlphanumeric().withMessage('Username must be alphanumeric'),
+//define registration route and logic
+app.post('/plp/users/registration', [
+    //validation check
+    check('email').isEmail().withMessage('Provide valid email address.'),
+    check('username').isAlphanumeric().withMessage('Invalid username. Provide aplhanumeric values.'),
 
-    // Custom validation to check if email and username are unique
-    check('email').custom(async (value) => {
-        const user = await User.getUserByEmail(value);
-        if (user) {
+    check('email').custom( async(value) => {
+        const exist = await User.getUserByEmail(value);
+        if(exist){
             throw new Error('Email already exists');
         }
     }),
-    check('username').custom(async (value) => {
-        const user = await User.getUserByUsername(value);
-        if (user) {
-            throw new Error('Username already exists');
+    check('username').custom( async(value) => {
+        const exist = await User.getUserByUsername(value);
+        if(exist){
+            throw new Error('Username already in use.');
         }
-    }),
-], async (req, res) => {
-    // Check for validation errors
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+    })
+], async (request, response) => {
+    //check for validation
+    const errors = validationResult(request);
+    if(!errors.isEmpty()){
+        return response.status(400).json({ errors: errors.array() });
     }
 
-    // Hash the password
+    //hash password
     const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+    const hashedPassword = await bcrypt.hash(request.body.password, saltRounds);
 
-    // Create a new user object
+    //define a new user object
     const newUser = {
-        email: req.body.email,
-        username: req.body.username,
-        password: hashedPassword,
-        full_name: req.body.full_name
-    };
+        full_name: request.body.full_name,
+        email: request.body.email,
+        username: request.body.username,
+        password: hashedPassword
+    }
 
-    // Insert user into MySQL
-    User.createUser(newUser, (error, results, fields) => {
-        if (error) {
-          console.error('Error inserting user: ' + error.message);
-          return res.status(500).json({ error: error.message });
+    //save new user
+    User.createUser(newUser, (error) => {
+        if(error){
+            console.error('An error occurred while saving the record: ' + error.message);
+            return response.status(500).json({ error: error.message });
         }
-        console.log('Inserted a new user with id ' + results.insertId);
-        res.status(201).json(newUser);
-      });
+        console.log('New user record saved!');
+        response.status(201).send('Registration successful!');
+    });
+
 });
 
-// Login route
-app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-    // Retrieve user from database
+//handle the login logic - authentication
+app.post('/api/user/login', (request, response) => {
+    const { username, password } = request.body;
+
     connection.query('SELECT * FROM users WHERE username = ?', [username], (err, results) => {
-        if (err) throw err;
-        if (results.length === 0) {
-            res.status(401).send('Invalid username or password');
+        if(err) throw err;
+        if(results.length === 0){
+            response.status(401).send('Invalid username or password.');
         } else {
             const user = results[0];
-            // Compare passwords
+            //compare passwords
             bcrypt.compare(password, user.password, (err, isMatch) => {
-                if (err) throw err;
-                if (isMatch) {
-                    // Store user in session
-                    req.session.user = user;
-                    res.send('Login successful');
+                if(err) throw err;
+                if(isMatch){
+                    //storing user data to the session variable
+                    request.session.user = user;
+                    response.status(200).json({ message: 'Login successful' });
                 } else {
-                    res.status(401).send('Invalid username or password');
+                    response.status(401).send('Invalid username or password.');
                 }
             });
         }
     });
 });
 
-//Dashboard route
-app.get('/dashboard', (req, res) => {
-    // Assuming you have middleware to handle user authentication and store user information in req.user
-    const userFullName = req.user.full_name;
-    res.render('dashboard', { fullName: userFullName });
+//handle authorization
+const userAuthenticated = (request, response, next) => {
+    if(request.session.user){
+        next();
+    } else {
+        response.redirect('/login');
+    }
+}
+
+//secure route
+app.get('/dashboard', userAuthenticated, (request, response) => {
+    response.status(200).json({ message: 'You are viewing a secured route.'});
 });
 
-// Start server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+//destroy session
+app.get('/logout', (request, response) => {
+    request.session.destroy();
+});
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
